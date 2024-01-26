bcafbd8f8717da69ca5c99bf23078310d87379ba
ahosseini06 <ali20060101@gmail.com>
2024-01-26 10:50:36 -0800

    user verification
    The /tournaments endpoint now only returns the data that belongs to an authenticated user.

--- PLAYMAKER-API/src/api/tournament/controllers/tournament.js
+++ PLAYMAKER-API/src/api/tournament/controllers/tournament.js
@@ -5,5 +5,30 @@
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
+const jwt = require('jwt-decode');
 
-module.exports = createCoreController('api::tournament.tournament');
+
+
+module.exports = createCoreController('api::tournament.tournament', ({strapi}) => ({    
+    async find(ctx) {
+        const authorizationHeader = ctx.headers.authorization;
+        const [scheme, token] = authorizationHeader.split(' ');
+        const decodedToken = jwt.jwtDecode(token);
+        
+
+        ctx.query =   
+        {
+        filters: {
+            user: {
+                id: {
+                    $eq: decodedToken.id
+                }
+            }
+        }
+        }
+
+        const {data, meta} = await super.find(ctx);
+        return {data, meta}
+    }
+
+}));
