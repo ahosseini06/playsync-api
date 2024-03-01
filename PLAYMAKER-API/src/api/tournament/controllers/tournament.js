a196aa95ea1b1aad5ccfe0fbfc0a6628c7dc4733
Aryan Singh <aryan.singh.lamba@gmail.com>
2024-03-01 09:14:31 -0800

    endpoint to check if tournament is open for registration

--- PLAYMAKER-API/src/api/tournament/controllers/tournament.js
+++ PLAYMAKER-API/src/api/tournament/controllers/tournament.js
@@ -8,24 +8,29 @@
 const { createCoreController } = require('@strapi/strapi').factories;
 const jwt = require('jwt-decode');
 
-module.exports = createCoreController('api::tournament.tournament', ({strapi}) => ({    
-    async find(ctx) {
-        const authorizationHeader = ctx.headers.authorization;
-        const [scheme, token] = authorizationHeader.split(' ');
-        const decodedToken = jwt.jwtDecode(token);
+module.exports = createCoreController('api::tournament.tournament', ({ strapi }) => ({
+  async find(ctx) {
+    const authorizationHeader = ctx.headers.authorization;
+    const [scheme, token] = authorizationHeader.split(' ');
+    const decodedToken = jwt.jwtDecode(token);
 
-        ctx.query = {
-            filters: {
-                user: {
-                    id: {
-                        $eq: decodedToken.id
-                    }
-                }
-            }
+    ctx.query = {
+      filters: {
+        user: {
+          id: {
+            $eq: decodedToken.id
+          }
         }
-
-        const {data, meta} = await super.find(ctx);
-        return {data, meta}
+      }
     }
 
+    const { data, meta } = await super.find(ctx);
+    return { data, meta }
+  },
+
+  async canRegister(ctx) {
+    const id = ctx.request.url.split("?id=");
+    const tournament = await strapi.entityService.findOne("api::tournament.tournament", id);
+    return tournament.status === 'scheduled'
+  }
 }));
