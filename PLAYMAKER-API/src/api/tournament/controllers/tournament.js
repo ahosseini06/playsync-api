57b2f5ed9d151cd7a2db27ea484a429b5a0de329
Aryan Singh <aryan.singh.lamba@gmail.com>
2024-03-05 17:13:00 -0800

    auto add user to tournament using jwt token

--- PLAYMAKER-API/src/api/tournament/controllers/tournament.js
+++ PLAYMAKER-API/src/api/tournament/controllers/tournament.js
@@ -90,12 +90,21 @@ module.exports = createCoreController('api::tournament.tournament', ({ strapi })
       field: "public_id",
       data: ctx.request.body.data
     })
+    //user retrieval
+    const authorizationHeader = ctx.headers.authorization;
+    if (!authorizationHeader) {
+      return ctx.badRequest('Authorization header is required');
+    }
+    const [scheme, token] = authorizationHeader.split(' ');
+    const userID = jwt.jwtDecode(token).id;
+    //tournament creation
     const response = await super.create(ctx);
     await strapi.entityService.update('api::tournament.tournament', response.data.id, {
       data: {
         event_dates: dates,
         private_id: privateID.toString(),
-        public_id: publicID
+        public_id: publicID,
+        user: userID
       }
     });
     return response
