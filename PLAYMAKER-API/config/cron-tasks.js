8ebebe8109be90dee57b6742b20e991de09cb43f
Aryan Singh <aryan.singh.lamba@gmail.com>
2024-03-06 10:35:37 -0800

    moved check in functionality to service

--- PLAYMAKER-API/config/cron-tasks.js
+++ PLAYMAKER-API/config/cron-tasks.js
@@ -1,6 +1,6 @@
 module.exports = {
   //activate tournaments on start date:
-  '*/30 * * * *': async ({ strapi }) => {
+  '*/1 * * * *': async ({ strapi }) => {
     const currentDate = new Date();
     currentDate.setSeconds(0);
     currentDate.setMilliseconds(0);
@@ -13,33 +13,7 @@ module.exports = {
       }
     });
     dates.forEach(async (d) => {
-      if (d.check_in_policy !== "none") {
-        const matches = await strapi.entityService.findMany('api::match.match', {
-          filters: {
-            tournament: d.tournament.id
-          },
-          populate: {
-            team_1: true,
-            team_2: true
-          }
-        })
-        const teams = matches.map(m => [m.team_1, m.team_2]).flat().filter(t=>t)
-        const uncheckedTeams = teams.filter(t => !t.checked_in)
-        if (uncheckedTeams.length > 0) {
-          /*if (d.check_in_policy === "delay") {
-            console.log("Cannot start tournament until all players have checked in")
-            await strapi.entityService.create('api::event-date.event-date', {
-              data: {
-                ...d,
-                tournament: d.tournament.id,
-                date: new Date(d.datetime.getTime() + 30 * 60000)
-              }
-            })
-            return
-          }*/
-          console.log("The following teams have not checked in: ", uncheckedTeams.map(p => p.name))
-        }
-      }
+      await strapi.service('api::tournament.tournament').manageCheckIn(d)
       const updates = {}
       if (d.status_action) {
         updates.status = d.status_action
--- PLAYMAKER-API/src/api/tournament/services/tournament.js
+++ PLAYMAKER-API/src/api/tournament/services/tournament.js
@@ -6,4 +6,39 @@
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::tournament.tournament');
+module.exports = createCoreService('api::tournament.tournament', ({ strapi }) => ({
+  async generatePools(tournament) {
+    //get teams
+    const teams = tournament.teams;
+  },
+
+  async manageCheckIn(d){
+    if (d.check_in_policy !== "none") {
+      const matches = await strapi.entityService.findMany('api::match.match', {
+        filters: {
+          tournament: d.tournament.id
+        },
+        populate: {
+          team_1: true,
+          team_2: true
+        }
+      })
+      const teams = matches.map(m => [m.team_1, m.team_2]).flat().filter(t=>t)
+      const uncheckedTeams = teams.filter(t => !t.checked_in)
+      if (uncheckedTeams.length > 0) {
+        /*if (d.check_in_policy === "delay") {
+          console.log("Cannot start tournament until all players have checked in")
+          await strapi.entityService.create('api::event-date.event-date', {
+            data: {
+              ...d,
+              tournament: d.tournament.id,
+              date: new Date(d.datetime.getTime() + 30 * 60000)
+            }
+          })
+          return
+        }*/
+        console.log("The following teams have not checked in: ", uncheckedTeams.map(p => p.name))
+      }
+    }
+  }
+}));
