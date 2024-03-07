47d330fc12d0ce260e0d0e12c65da993cb72adea
Aryan Singh <aryan.singh.lamba@gmail.com>
2024-03-06 18:39:30 -0800

    auto create players and coaches on team creation

--- PLAYMAKER-API/src/api/team/controllers/team.js
+++ PLAYMAKER-API/src/api/team/controllers/team.js
@@ -6,4 +6,33 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::team.team');
+module.exports = createCoreController('api::team.team', ({ strapi }) => ({
+  async create(ctx) {
+    if (!ctx.request.body.data) {
+      return ctx.badRequest('Data is required');
+    }
+    //player creation
+    const players = await Promise.all(ctx.request.body.data.players.map(async p => 
+      await strapi.entityService.create('api::player.player', {
+        data: p,
+        publishedAt: new Date()
+      })
+    ));
+    //coach creation
+    const coaches = await Promise.all(ctx.request.body.data.coaches.map(async c =>
+      await strapi.entityService.create('api::coach.coach', {
+        data: c,
+        publishedAt: new Date()
+      })
+    ))
+    //team creation
+    const response = await super.create(ctx);
+    await strapi.entityService.update('api::team.team', response.data.id, {
+      data: {
+        players: players.map(p => p.id),
+        coaches: coaches.map(c => c.id)
+      }
+    });
+    return response
+  }
+}));
