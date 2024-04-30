efd8c3ec405789717a7846c42ec3afa8b758501a
Aryan Singh <aryan.singh.lamba@gmail.com>
2024-04-29 17:30:31 -0700

    auto assign coach to user based on jwt

--- PLAYMAKER-API/src/api/coach/controllers/coach.js
+++ PLAYMAKER-API/src/api/coach/controllers/coach.js
@@ -4,6 +4,24 @@
  * coach controller
  */
 
+const jwt = require('jwt-decode');
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::coach.coach');
+module.exports = createCoreController('api::coach.coach', (({ strapi }) => ({
+  async create(ctx) {
+    //user retrieval
+    const authorizationHeader = ctx.headers.authorization;
+    if (!authorizationHeader) {
+      return ctx.badRequest('Authorization header is required');
+    }
+    const [scheme, token] = authorizationHeader.split(' ');
+    const userID = jwt.jwtDecode(token).id;
+    const response = await super.create(ctx);
+    const newResponse = await strapi.entityService.update('api::coach.coach', response.data.id, {
+      data: {
+        user: userID
+      }
+    });
+    return newResponse
+  }
+}));
